39e8c624c20ffe73a741d534f11027d13b3feac0
you <macbookair@Dolphin.local>
2020-01-13 00:16:52 -0600

    Creat project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import FormContainer from "./js/components/container/FormContainer.jsx";
+console.log("Good")
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+let a=<div>
+  <h1>good</h1>
+  <p>lormesadfasd adsf asdf asdewfasdf aeafsd5</p>
+  </div>
+ReactDOM.render(
+  a,
+  document.getElementById("demo")
+)
